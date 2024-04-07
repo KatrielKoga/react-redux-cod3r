590425ae763f2172d4f3ee3df37d4c32385693bf
KatrielKoga <katriel.koga.2012@hotmail.com>
2024-04-07 09:30:58 -0300

    simple frontend todo app

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PageHeader from '../templates/pageHeader';
+
+export default props => (
+	<div>
+		<PageHeader name="Sobre" small="Nós" />
+
+		<h2>Nossa História</h2>
+		<p>
+			Aliquip cupidatat aliqua do duis cillum do consequat reprehenderit mollit
+			mollit.
+		</p>
+		<h2>Missão e Visão</h2>
+		<p>
+			Aliquip cupidatat aliqua do duis cillum do consequat reprehenderit mollit
+			mollit.
+		</p>
+		<h2>Imprensa</h2>
+		<p>
+			Aliquip cupidatat aliqua do duis cillum do consequat reprehenderit mollit
+			mollit.
+		</p>
+	</div>
+);

4280459368d9f9624b3f81c4270b45dae85632a6
KatrielKoga <katriel.koga.2012@hotmail.com>
2024-04-08 07:28:05 -0300

    start of simple project about redux

---
+++ redux-simples/src/components/Media.jsx
@@ -0,0 +1,15 @@
+import Card from './Card';
+import React from 'react';
+
+export default function Media(props) {
+	return (
+		<Card title="Média dos Números" green>
+			<div>
+				<span>
+					<span>Resultado: </span>
+					<strong>5</strong>
+				</span>
+			</div>
+		</Card>
+	);
+}
---
+++ redux-simples/src/components/Sorteio.jsx
@@ -0,0 +1,15 @@
+import Card from './Card';
+import React from 'react';
+
+export default function Sorteio(props) {
+	return (
+		<Card title="Sorteio de um Número" purple>
+			<div>
+				<span>
+					<span>Resultado: </span>
+					<strong>8</strong>
+				</span>
+			</div>
+		</Card>
+	);
+}
